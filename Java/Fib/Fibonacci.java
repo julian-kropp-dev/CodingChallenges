b79685f4765a13183dedac3cf3c9ee2b02430bc2
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2022-07-19 08:10:56 +0200

    created code for Fib Recursive, Parallel, Dynamic & Dynamic-Paralell

---
+++ Java/Fib/Fibonacci.java
@@ -0,0 +1,146 @@
+
+
+import java.util.HashMap;
+
+public abstract class Fibonacci {
+	
+	public abstract long calculate(int n);
+	
+}
+
+
+class FibonacciRecursive extends Fibonacci {
+
+	@Override
+	public long calculate(int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		} else {
+			return calculate(n - 1) + calculate(n - 2);
+		}
+	}
+
+}
+
+
+
+class FibonacciParallel extends Fibonacci {
+	@Override
+	public long calculate(int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		} else {
+			FibonacciThread fib1 = new FibonacciThread(n - 1);
+			FibonacciThread fib2 = new FibonacciThread(n - 2);
+			fib1.start();
+			fib2.start();
+			try {
+				fib1.join();
+				fib2.join();
+			} catch (InterruptedException e) {
+				return -1;
+			}
+			return fib1.getResult() + fib2.getResult();
+		}
+	}
+
+	private class FibonacciThread extends Thread {
+		private int n;
+		private long result;
+
+		public FibonacciThread(int n) {
+			this.n = n;
+		}
+
+		@Override
+		public void run() {
+			result = calculate(n);
+		}
+
+		public long getResult() {
+			return result;
+		}
+	}
+}
+
+class FibonacciDynamic extends Fibonacci {
+	private static final HashMap<Integer, Long> myResult = new HashMap<>();
+
+	public FibonacciDynamic() {
+		myResult.put(0, (long) 0);
+		myResult.put(1, (long) 1);
+	}
+
+	@Override
+	public long calculate(int n) {
+
+		if (myResult.containsKey(n)) {
+			return myResult.get(n);
+		} else {
+
+			long result = calculate(n - 2) + calculate(n - 1);
+			myResult.put(n, result);
+			return result;
+		}
+	}
+}
+
+class FibonacciDynamicParallel extends Fibonacci {
+	private static final HashMap<Integer, Long> myResult = new HashMap<>();
+
+	public FibonacciDynamicParallel() {
+		myResult.put(0, (long) 0);
+		myResult.put(1, (long) 1);
+	}
+
+	@Override
+	public long calculate(int n) {
+		return calculateDynamic2(n);
+	}
+
+	private long calculateDynamic2(int n) {
+		long result;
+		if (myResult.containsKey(n)) {
+			return myResult.get(n);
+		} else {
+			FibonacciThread2 thread1 = new FibonacciThread2(n - 1);
+			FibonacciThread2 thread2 = new FibonacciThread2(n - 2);
+			thread1.start();
+			thread2.start();
+			try {
+				thread1.join();
+				thread2.join();
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			result = thread1.getResult() + thread2.getResult();
+			myResult.put(n, result);
+	}return result;}
+
+	class FibonacciThread2 extends Thread {
+			private int n;
+			private long result;
+
+			public FibonacciThread2(int n) {
+				this.n = n;
+			}
+
+			@Override
+			public void run() {
+				result = calculate(n);
+			}
+
+			public long getResult() {
+				return result;
+			}
+		}
+
+
+}
+
+
+
+
+
+
---
+++ Java/Fib/FibonacciTest.java
@@ -0,0 +1,90 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FibonacciTest {
+
+	private static final int[] NUMBERS = new int[] { 3, 5, 8, 12, 9, 18, 15, 10, 7, 11, 20 };
+	FibonacciRecursive fibrec = new FibonacciRecursive();
+
+	FibonacciParallel fibparal = new FibonacciParallel();
+
+	FibonacciDynamic fibdyn = new FibonacciDynamic();
+
+	FibonacciDynamicParallel fibdynpar = new FibonacciDynamicParallel();
+
+	@Test
+	void test() {
+
+		for (int i = 0; i < NUMBERS.length; i++) {
+			long result0 = fibrec.calculate(NUMBERS[i]);
+			long result1 = fibparal.calculate(NUMBERS[i]);
+			long result2 = fibdyn.calculate(NUMBERS[i]);
+			long result3 = fibdynpar.calculate(NUMBERS[i]);
+			assertEquals(result0, result1);
+			assertEquals(result1, result2);
+			assertEquals(result2, result3);
+		}
+
+	}
+
+	@Test
+	void timeRecursive() {
+		Long start = System.currentTimeMillis();
+		for (int i = 0; i < NUMBERS.length; i++) {
+			fibrec.calculate(NUMBERS[i]);
+		}
+		System.out.println("Recursive: " + (System.currentTimeMillis() - start) );
+
+	}
+
+	@Test
+	void timeParallel() {
+		Long start = System.currentTimeMillis();
+		for (int i = 0; i < NUMBERS.length; i++) {
+			fibparal.calculate(NUMBERS[i]);
+		}
+		System.out.println("Parallel: " + (System.currentTimeMillis() - start) );
+
+	}
+
+	@Test
+	void timeDynamic() {
+		Long start = System.currentTimeMillis();
+		for (int i = 0; i < NUMBERS.length; i++) {
+			fibdyn.calculate(NUMBERS[i]);
+		}
+		System.out.println("Dynamic: " + (System.currentTimeMillis() - start) );
+
+	}
+
+	@Test
+	void timeDynamicParallel() {
+		Long start = System.currentTimeMillis();
+		for (int i = 0; i < NUMBERS.length; i++) {
+			fibdynpar.calculate(NUMBERS[i]);
+		}
+		System.out.println("ParallelDynamic: " + (System.currentTimeMillis() - start) );
+
+	}
+}
+
+
+/*
+Timing-Ergebnisse:
+Recursive: 1ms
+Parallel: 16943ms
+Dynamic: 0ms
+ParallelDynamic: 0ms (bei anderen Messungen etwas langsamer als Dynamic)
+*/
+
+/*
+Die Timing-Ergebnisse entsprechen nicht der Erwartung, dass Parallelisierung beschleunigt.
+Das hat mehrere Gründe u.a.:
+- Die Erzeugung von Threads ist sehr viel "teurer" als ein Methodenaufruf
+- Die Threads tun inhaltlich fast garnichts (nur Addition), d.h. die Thread-Erzeugung/-Vernichtung ist die maßgebliche Laufzeit
+- Bei der parallelen, rekursiven Methode werden tausende von Threads erzeugt, d.h.
+	- massiver Zeitaufwand fuer Thread-Erzeugung
+	- es kann fast nichts wirklich parallel stattfinden
+*/
